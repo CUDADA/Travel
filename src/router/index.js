5f9e9fab24284992f215416006f3da6f9eb24bd9
Jason <13716897712@163.com>
2018-08-09 17:43:34 +0800

    add asyload

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ export default new Router({
     path: '/',
     name: 'Home',
     component: Home
+    // component() => import('@/pages/home/Home')
   }, {
     path: '/city',
     name: 'City',
